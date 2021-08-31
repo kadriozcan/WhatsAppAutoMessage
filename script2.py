0cdac172db5270a562c95f91f29be75bd23508e4
Kadri Ozcan <kadriozcan38@gmail.com>
2021-08-31 12:45:47 +0300

    ok3

--- script2.py
+++ script2.py
@@ -5,6 +5,7 @@ message = "Deneme mesaji"
 data = pandas.read_csv('list.csv')
 data_dict = data.to_dict('list')
 numbers = data_dict['Numbers']
+message = data_dict['Message']
 
 for number in numbers:  
     pywhatkit.sendwhatmsg_instantly(f"+90{number}", "Deneme Mesaji", 5, True, 3)
