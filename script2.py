9bb2aeed2c1f3b0d19331b1731220c7165f7b8f1
Kadri Ozcan <kadriozcan38@gmail.com>
2021-08-31 12:30:05 +0300

    ok

---
+++ script2.py
@@ -0,0 +1,10 @@
+import webbrowser, pyautogui, time, pandas, pywhatkit
+
+message = "Deneme mesaji"
+
+data = pandas.read_csv('list.csv')
+data_dict = data.to_dict('list')
+numbers = data_dict['Numbers']
+
+for number in numbers:  
+    pywhatkit.sendwhatmsg_instantly(f"+90{number}", "Deneme Mesaji", 5, True, 3)
